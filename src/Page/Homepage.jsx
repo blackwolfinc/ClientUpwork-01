d8022d64e60b40bc884e0fa0652e6f006daa44ea
Black Wolf Team <47728533+blackwolfinc@users.noreply.github.com>
2022-11-24 22:23:06 +0700

    frist setup

---
+++ src/Page/Homepage.jsx
@@ -0,0 +1,11 @@
+import "../assets/css/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">tes</header>
+    </div>
+  );
+}
+
+export default App;
