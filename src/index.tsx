82c447a74dbd9876dd8eeafa9da64f298fd35fb9
Black Wolf Team <47728533+blackwolfinc@users.noreply.github.com>
2022-12-03 13:59:11 +0700

    fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { Homepage } from "./page/Homepage";
 import "../src/assets/css/index.css";
+import { Homepage } from "./page/Homepage";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-{
-  /* Frist Page Render */
-}
+
 root.render(
   <React.StrictMode>
     <Homepage />
